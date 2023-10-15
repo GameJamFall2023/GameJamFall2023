eb4d6601da60362534beb1ef9d060e4669ac96bb
Givo <giv0online@gmail.com>
2023-10-14 19:15:45 -0500

    pause

--- Scripts/Game Manager.gd
+++ Scripts/Game Manager.gd
@@ -10,6 +10,10 @@ var batt4 = false;
 var socks = true;
 var soda = false;
 
+var paused = false;
+
+@onready var pauseScreen = $Camera2D/Paused;
+
 func _ready():
 	if !Instance:
 		Instance = self;
@@ -18,7 +22,19 @@ func _ready():
 
 
 func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		paused = !paused;
+		
 	if !DisplayServer.window_is_focused():
+		paused = true;
+		
+	if paused:
 		Engine.time_scale = 0;
+		pauseScreen.visible = true;
 	else:
 		Engine.time_scale = 1;
+		pauseScreen.visible = false;
+
+
+func _on_unpause_pressed():
+	paused = false;
